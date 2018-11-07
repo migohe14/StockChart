2b322de1d344422e6bdafaf528502940ec5ef065
migohe14 <migohe14@gmail.com>
2018-11-07 11:59:30 +0100

    vue and socket.io

---
+++ stock-chart/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 offset-md-3">
+          <chart />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Chart from './components/Chart.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Chart
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ stock-chart/src/components/Chart.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="card mt-3">
+      <div class="card-body">
+          <div class="card-title">
+              <h3>Stock Chart</h3>
+              <hr>
+          </div>
+          <div class="card-body">
+              <div class="messages">
+                  
+              </div>
+          </div>
+      </div>
+      <div class="card-footer">
+          <form @submit.prevent="getData">
+              <div class="gorm-group">
+                  <label for="user">User:</label>
+                  <input type="text" v-model="user" class="form-control">
+              </div>
+            
+              <button type="submit" class="btn btn-success">Get data</button>
+          </form>
+      </div>
+  </div>
+</template>
+
+<script>
+import io from 'socket.io-client';
+
+export default {
+       data() {
+        return {
+           
+            stockprices: [],
+            socket : io('localhost:3001')
+        }
+    },
+    methods: {
+        getData(e) {
+            e.preventDefault();
+        }
+    },
+        mounted() {
+        this.socket.on('MESSAGE', (data) => {
+            // this.stockprices = [...this.messages, data];
+            // // you can also do this.messages.push(data)
+        });
+    }
+}
+</script>
+
